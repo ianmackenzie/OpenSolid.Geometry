305b36bdb7142b1f043e260e2f4065289c27c122
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2017-04-03 17:57:04 -0400

    Simplify a few functions
    Try to avoid using components - stick with clean, 'obviously correct'
    implementations for now

--- src/Axis2d.fs
+++ src/Axis2d.fs
@@ -1,8 +1,9 @@
 [<CompilationRepresentationAttribute(CompilationRepresentationFlags.ModuleSuffix)>]
 module OpenSolid.Axis2d
 
-open OpenSolid.Point2d
+open OpenSolid.Vector2d
 open OpenSolid.Direction2d
+open OpenSolid.Point2d
 
 type Axis2d(originPoint : Point2d, direction : Direction2d) =
     member inline internal this._originPoint =
@@ -19,21 +20,16 @@ type Axis2d(originPoint : Point2d, direction : Direction2d) =
 
 type Point2d with
     static member along axis distance =
-        let (x0, y0) = Point2d.coordinates (Axis2d.originPoint axis)
-        let (dx, dy) = Direction2d.components (Axis2d.direction axis)
-        Point2d(x0 + distance * dx, y0 + distance * dy)
+        Axis2d.originPoint axis + distance * Axis2d.direction axis
 
-    static member distanceAlong axis point =
-        let (px, py) = Point2d.coordinates point
-        let (x0, y0) = Point2d.coordinates (Axis2d.originPoint axis)
-        let (dx, dy) = Direction2d.components (Axis2d.direction axis)
-        (px - x0) * dx + (py - y0) * dy
+    static member distanceAlong axis =
+        Point2d.vectorFrom (Axis2d.originPoint axis)
+            |> Vector2d.componentIn (Axis2d.direction axis)
 
     static member signedDistanceFrom axis point =
-        let (px, py) = Point2d.coordinates point
-        let (x0, y0) = Point2d.coordinates (Axis2d.originPoint axis)
-        let (dx, dy) = Direction2d.components (Axis2d.direction axis)
-        (py - y0) * dx - (px - x0) * dy
+        let displacement = Point2d.vectorFrom (Axis2d.originPoint axis)
+        let directionVector = Direction2d.toVector (Axis2d.direction axis)
+        Vector2d.crossProduct directionVector displacement
 
 
 
--- src/Frame2d.fs
+++ src/Frame2d.fs
@@ -1,8 +1,9 @@
 [<CompilationRepresentationAttribute(CompilationRepresentationFlags.ModuleSuffix)>]
 module OpenSolid.Frame2d
 
-open OpenSolid.Point2d
+open OpenSolid.Vector2d
 open OpenSolid.Direction2d
+open OpenSolid.Point2d
 open OpenSolid.Axis2d
 
 type Frame2d(originPoint : Point2d, xDirection : Direction2d, yDirection : Direction2d) =
@@ -37,9 +38,9 @@ type Frame2d(originPoint : Point2d, xDirection : Direction2d, yDirection : Direc
         Axis2d (Frame2d.originPoint frame, Frame2d.yDirection frame)
 
     static member isRightHanded frame =
-        let (x1, y1) = Direction2d.components (Frame2d.xDirection frame)
-        let (x2, y2) = Direction2d.components (Frame2d.yDirection frame)
-        x1 * y2 - y1 * x2 >= 0.0
+        let xDirectionVector = Direction2d.toVector (Frame2d.xDirection frame)
+        let yDirectionVector = Direction2d.toVector (Frame2d.yDirection frame)
+        Vector2d.crossProduct xDirectionVector yDirectionVector >= 0.0
 
     static member flipX frame =
         let originPoint = Frame2d.originPoint frame
@@ -67,12 +68,10 @@ type Frame2d(originPoint : Point2d, xDirection : Direction2d, yDirection : Direc
             Frame2d (originPoint, xDirection, yDirection)
 
 type Point2d with
-    static member in_ frame coordinates =
-        let (x, y) = coordinates
-        let (x0, y0) = Point2d.coordinates (Frame2d.originPoint frame)
-        let (ux, uy) = Direction2d.components (Frame2d.xDirection frame)
-        let (vx, vy) = Direction2d.components (Frame2d.yDirection frame)
-        Point2d(x0 + x * ux + y * vx, y0 + x * uy + y * vy)
+    static member in_ frame (x, y) =
+        Frame2d.originPoint frame
+            + x * Frame2d.xDirection frame
+            + y * Frame2d.yDirection frame
 
 // rotateAround
 // translateBy
