41ddf34df7580f0eab2f6698b40804f2c230439d
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2017-04-05 12:14:29 -0400

    Add equality/hash functions where appropriate

--- src/Types.fs
+++ src/Types.fs
@@ -5,6 +5,27 @@ type Vector2d(xComponent: float, yComponent: float) =
 
     member this.YComponent = yComponent
 
+    override this.GetHashCode() =
+        hash (this.XComponent, this.YComponent)
+
+    override this.Equals(other: obj) =
+        match other with
+        | :? Vector2d as vector -> this.Equals(vector)
+        | _ -> false
+
+    member this.Equals(vector: Vector2d) =
+        let x1 = this.XComponent
+        let y1 = this.YComponent
+        let x2 = vector.XComponent
+        let y2 = vector.YComponent
+        x1 = x2 && y1 = y2
+
+    static member op_Equality (firstVector: Vector2d, secondVector: Vector2d) =
+        firstVector.Equals(secondVector)
+
+    static member op_Inequality (firstVector: Vector2d, secondVector: Vector2d) =
+        not (firstVector.Equals(secondVector))
+
     static member (~-) (vector: Vector2d) =
         Vector2d(-vector.XComponent, -vector.YComponent)
 
@@ -42,8 +63,26 @@ type Direction2d(xComponent: float, yComponent: float) =
 
     member this.YComponent = yComponent
 
+    override this.GetHashCode() =
+        hash (this.XComponent, this.YComponent)
+
+    override this.Equals(other: obj) =
+        match other with
+        | :? Direction2d as direction -> this.Equals(direction)
+        | _ -> false
+
+    member this.Equals(direction: Direction2d) =
         let x1 = this.XComponent
         let y1 = this.YComponent
+        let x2 = direction.XComponent
+        let y2 = direction.YComponent
+        x1 = x2 && y1 = y2
+
+    static member op_Equality (firstDirection: Direction2d, secondDirection: Direction2d) =
+        firstDirection.Equals(secondDirection)
+
+    static member op_Inequality (firstDirection: Direction2d, secondDirection: Direction2d) =
+        not (firstDirection.Equals(secondDirection))
 
     static member (~-) (direction: Direction2d) =
         Direction2d(-direction.XComponent, -direction.YComponent)
@@ -62,6 +101,27 @@ type Point2d(xCoordinate: float, yCoordinate: float) =
     member this.XCoordinate = xCoordinate
 
     member this.YCoordinate = yCoordinate
+
+    override this.GetHashCode() =
+        hash (this.XCoordinate, this.YCoordinate)
+
+    override this.Equals(other: obj) =
+        match other with
+        | :? Point2d as point -> this.Equals(point)
+        | _ -> false
+
+    member this.Equals(point: Point2d) =
+        let x1 = this.XCoordinate
+        let y1 = this.YCoordinate
+        let x2 = point.XCoordinate
+        let y2 = point.YCoordinate
+        x1 = x2 && y1 = y2
+
+    static member op_Equality (firstPoint: Point2d, secondPoint: Point2d) =
+        firstPoint.Equals(secondPoint)
+
+    static member op_Inequality (firstPoint: Point2d, secondPoint: Point2d) =
+        not (firstPoint.Equals(secondPoint))
 
     static member (+) (point: Point2d, vector: Vector2d) =
         let px = point.XCoordinate
