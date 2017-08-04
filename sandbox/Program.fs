8c00dc547f1076f01089145cf3de03315dc7e1a8
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2017-08-04 18:01:00 -0400

    Add JSON encoding/decoding

---
+++ sandbox/Program.fs
@@ -0,0 +1,15 @@
+﻿open System
+
+open Chiron
+
+open OpenSolid
+open OpenSolid.Vector2d
+
+[<EntryPoint>]
+let main argv =
+    let vector = Vector2d(2.0, 3.0)
+    printfn "%s" (Json.format (Json.serialize vector))
+    let json = """{"originPoint": {"x": 3, "y": 4}, "direction": {"x": 1, "yz": 0}}"""
+    let deserialized : Choice<Axis2d, string> = Json.tryDeserialize (Json.parse json)
+    printfn "%A" deserialized
+    0
--- src/Types.fs
+++ src/Types.fs
@@ -1,5 +1,7 @@
 namespace OpenSolid
 
+open Chiron
+
 type Components2d =
     float * float
 
@@ -23,3 +25,51 @@ type Axis2d =
 
 type Frame2d =
     Frame2d of originPoint : Point2d * xDirection : Direction2d * yDirection : Direction2d
+
+type Vector2d with
+    static member ToJson (Vector2d (x, y)) = json {
+        do! Json.write "x" x
+        do! Json.write "y" y
+    }
+
+    static member FromJson (_: Vector2d) = json {
+        let! x = Json.read "x"
+        let! y = Json.read "y"
+        return Vector2d (x, y)
+    }
+
+type Direction2d with
+    static member ToJson (Direction2d (x, y)) = json {
+        do! Json.write "x" x
+        do! Json.write "y" y
+    }
+
+    static member FromJson (_: Direction2d) = json {
+        let! x = Json.read "x"
+        let! y = Json.read "y"
+        return Direction2d (x, y)
+    }
+
+type Point2d with
+    static member ToJson (Point2d (x, y)) = json {
+        do! Json.write "x" x
+        do! Json.write "y" y
+    }
+
+    static member FromJson (_: Point2d) = json {
+        let! x = Json.read "x"
+        let! y = Json.read "y"
+        return Point2d (x, y)
+    }
+
+type Axis2d with
+    static member ToJson (Axis2d(originPoint, direction)) = json {
+        do! Json.write "originPoint" originPoint
+        do! Json.write "direction" direction
+    }
+
+    static member FromJson (_: Axis2d) = json {
+        let! originPoint = Json.read "originPoint"
+        let! direction = Json.read "direction"
+        return Axis2d(originPoint, direction)
+    }
